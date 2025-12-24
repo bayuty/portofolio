875e715b391b5dfb46188323dce15719f14203e5
bayuty <90265194+bayuty@users.noreply.github.com>
2025-12-24 16:03:31 +0700

    Update Projects.tsx

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -82,6 +82,8 @@ const Projects: React.FC = () => {
                 <img 
                   src={project.image} 
                   alt={project.title} 
+                  loading="lazy"
+                  decoding="async"
                   className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
                 />
                 
